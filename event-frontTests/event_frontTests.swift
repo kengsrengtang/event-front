b3a821d652c586f0bad09336e744c0b70df16271
kengtang <kengtang@cisco.com>
2015-08-03 19:38:06 -0700

    init event-front

---
+++ event-frontTests/event_frontTests.swift
@@ -0,0 +1,36 @@
+//
+//  event_frontTests.swift
+//  event-frontTests
+//
+//  Created by kengsrengtang on 8/3/15.
+//  Copyright (c) 2015 event. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class event_frontTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
